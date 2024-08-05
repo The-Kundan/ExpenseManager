1b47b59c89ba74a9919a20e2abc50deda3904f5f
The-Kundan <kundan17832863@gmail.com>
2024-08-05 19:33:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.expensemanager;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expensemanager.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle("Transaction");
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
